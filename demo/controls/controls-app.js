206b714a6e8f77389ae2f908d0687bd0ba185966
Karl Waclawek <karl@waclawek.net>
2021-05-09 23:32:53 -0400

    components - more work on controls demo.

--- demo/controls/controls-app.js
+++ demo/controls/controls-app.js
@@ -1,8 +1,13 @@
 import { Queue, priorities } from '@nx-js/queue-util/dist/es.es6.js';
 import { html } from 'lit';
 import { LitMvvmElement, css } from '@kdsoft/lit-mvvm';
+import tailwindStyles from '@kdsoft/lit-mvvm-components/styles/tailwind-styles.js';
+import checkboxStyles from '@kdsoft/lit-mvvm-components/styles/kdsoft-checkbox-styles.js';
+import fontAwesomeStyles from '@kdsoft/lit-mvvm-components/styles/fontawesome/css/all-styles.js';
 
 import { KdSoftChecklistModel } from '@kdsoft/lit-mvvm-components';
+import KdSoftDropdownModel from '@kdsoft/lit-mvvm-components/kdsoft-dropdown-model.js';
+import KdSoftDropdownChecklistConnector from '@kdsoft/lit-mvvm-components/kdsoft-dropdown-checklist-connector.js';
 
 class ControlsApp extends LitMvvmElement {
   constructor() {
@@ -14,6 +19,21 @@ class ControlsApp extends LitMvvmElement {
       true,
       item => item.name
     );
+    this.dropDownModel = new KdSoftDropdownModel();
+    this.checklistConnector = new KdSoftDropdownChecklistConnector(
+      () => this.renderRoot.getElementById('ddown'),
+      () => this.renderRoot.getElementById('clist'),
+      () => this.getChecklistText()
+    );
+  }
+
+  getChecklistText() {
+    let result = null;
+    for (const selEntry of this.checklistModel.selectedEntries) {
+      if (result) result += `, ${selEntry.item.name}`;
+      else result = selEntry.item.name;
+    }
+    return result;
   }
 
   getChecklistItemTemplate(item) {
@@ -51,9 +71,14 @@ class ControlsApp extends LitMvvmElement {
 
   static get styles() {
     return [
+      tailwindStyles,
+      checkboxStyles,
+      fontAwesomeStyles,
       css`
         #container {
-          display: block;
+          display: grid;
+          grid-template-columns: auto auto;
+          grid-gap: .5em;
         }
         #check-list {
           position: relative;
@@ -64,15 +89,23 @@ class ControlsApp extends LitMvvmElement {
           white-space: nowrap;
           overflow: hidden;
           text-overflow: ellipsis;
+          min-height: 2em;
         }
         #check-list > p:hover {
           overflow: initial;
           text-overflow: initial;
         }
-        kdsoft-checklist {
+        /* kdsoft-checklist {
           width: 100%;
           border: 1px solid grey;
           display: block;
+        } */
+        kdsoft-dropdown {
+          min-width: 200px;
+        }
+
+        kdsoft-checklist {
+          min-width: 200px;
         }
       `
     ];
@@ -89,14 +122,24 @@ class ControlsApp extends LitMvvmElement {
       </style>
       <div id="container">
         <div id="check-list">
-          <p>
-            <label for="selected-items">Selected:</label>
-            <span id="selected-items">${selectedItemsText}</span>
-          </p>
+          <h1 class="font-bold text-xl mb-2 text-center">Plain Checklist</h1>
           <kdsoft-checklist
+            id="just-clist"
             .model=${this.checklistModel}
             .getItemTemplate=${item => this.getChecklistItemTemplate(item)}
-            show-checkboxes></kdsoft-checklist>
+            allow-drag-drop show-checkboxes></kdsoft-checklist>
+        </div>
+        <div id="drop-down">
+        <h1 class="font-bold text-xl mb-2 text-center">Checklist in Dropdown</h1>
+          <kdsoft-dropdown id="ddown" class="py-0"
+            .model=${this.dropDownModel} .connector=${this.checklistConnector}>
+            <kdsoft-checklist
+              id="clist"
+              class="text-black"
+              .model=${this.checklistModel}
+              .getItemTemplate=${item => html`${item.name}`}>
+            </kdsoft-checklist>
+          </kdsoft-dropdown>
         </div>
       </div>
     `;
