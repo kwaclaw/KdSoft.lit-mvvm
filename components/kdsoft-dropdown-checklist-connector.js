dab5ae25f733b433bb1e371070bc63e028020a1e
Karl Waclawek <karl@waclawek.net>
2021-10-02 23:09:25 -0400

    Minor fix in connector.

--- components/kdsoft-dropdown-checklist-connector.js
+++ components/kdsoft-dropdown-checklist-connector.js
@@ -2,9 +2,9 @@ import { observe, unobserve } from '@nx-js/observer-util/dist/es.es6.js';
 import KdSoftDropdownConnector from './kdsoft-dropdown-connector.js';
 
 function _getSelectedText(clm) {
-  let result = null;
+  let result = '';
   for (const selEntry of clm.selectedEntries) {
-    if (result === null) result += `, ${selEntry.item.name}`;
+    if (result === '') result += `, ${selEntry.item.name}`;
     else result = selEntry.item.name;
   }
   return result;
