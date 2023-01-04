d8218779470e7f2d27c97fadbafcb5b4db71e783
Karl Waclawek <karl@waclawek.net>
2023-01-04 17:38:34 -0500

    demo/controls: reworked kds-drag-drop-provider to fix issues with nested events, and to handle data-drop-mode options.

--- demo/controls/kds-drag-drop-provider.js
+++ demo/controls/kds-drag-drop-provider.js
@@ -1,62 +1,133 @@
+function getDropElement(path) {
+  let dropElement = null;
+  for (let indx = 0; indx < path.length; indx += 1) {
+    const pathElement = path[indx];
+    if (pathElement.dataset?.dropMode) {
+      dropElement = pathElement;
+      break;
+    }
+  }
+  return dropElement;
+}
+
+function clearDropClasses(element) {
+  element.classList.remove('kds-droppable');
+  element.classList.remove('kds-droppable-before');
+  element.classList.remove('kds-droppable-after');
+}
+
 function dragStart(e) {
-  e.stopPropagation();
+  e.stopPropagation(); // dont want the same event further up
   // only text data, no objects!
   e.dataTransfer.setData('text/plain', this._getItemId(e.currentTarget));
   e.dataTransfer.effectAllowed = 'move';
 }
 
 function dragOver(e) {
-  e.preventDefault();
+  e.stopPropagation(); // dont want the same event further up
+  const dropElement = getDropElement(e.path);
+  if (dropElement) {
+    e.preventDefault();
+  }
 }
 
-// need to maintain a drag enter counter for the item, as the drag enter/leave event happen when a child
-// node is being moved over and we know only that we left the parent element when the counter reaches 0.
+// this happens also for the parent element when leaving a child element;
+// we need to maintain a drag enter counter for the item, as the drag enter/leave event happens when
+// a child node is being moved over and we know only that we left the parent element when the counter reaches 0.
 function dragEnter(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  e.dataTransfer.dropEffect = 'move';
-
+  e.stopPropagation(); // dont want the same event further up
   const dragEnterCount = e.currentTarget._kdsDragEnterCount || 0;
   if (dragEnterCount <= 0) {
     e.currentTarget._kdsDragEnterCount = 1;
-    e.currentTarget.classList.add('kds-droppable');
   } else {
     e.currentTarget._kdsDragEnterCount = dragEnterCount + 1;
   }
-}
 
-function dragLeave(e) {
-  e.stopPropagation();
-  e.preventDefault();
+  const dropElement = getDropElement(e.path);
+  if (dropElement) {
+    e.preventDefault();
 
-  const dragEnterCount = e.currentTarget._kdsDragEnterCount || 0;
-  e.currentTarget._kdsDragEnterCount = dragEnterCount - 1;
+    e.dataTransfer.dropEffect = 'move';
 
-  if (e.currentTarget._kdsDragEnterCount <= 0) {
-    e.currentTarget.classList.remove('kds-droppable');
+    const element = e.currentTarget;
+    element._kdsDropMode = dropElement.dataset.dropMode;
+    switch (dropElement.dataset.dropMode) {
+      case 'before':
+        element.classList.remove('kds-droppable');
+        element.classList.remove('kds-droppable-after');
+        element.classList.add('kds-droppable-before');
+        break;
+      case 'after':
+        element.classList.remove('kds-droppable');
+        element.classList.remove('kds-droppable-before');
+        element.classList.add('kds-droppable-after');
+        break;
+      default:
+        element.classList.remove('kds-droppable-before');
+        element.classList.remove('kds-droppable-after');
+        element.classList.add('kds-droppable');
+        break;
+    }
+
+    console.log('Enter: ', element);
   }
 }
 
+function checkLeave(element) {
+  const dragEnterCount = element._kdsDragEnterCount;
+  // ignore if there was no prior dragenter event
+  if (dragEnterCount < 0) {
+    return false;
+  }
+
+  const newDragEnterCount = dragEnterCount - 1;
+  if (newDragEnterCount < 1) {
+    console.log('Leave: ', element);
+    element._kdsDragEnterCount = 0;
+    return true;
+  }
+  element._kdsDragEnterCount = newDragEnterCount;
+  return false;
+}
+
+// this happens also for the parent element when entering a child element
+function dragLeave(e) {
+  e.stopPropagation(); // dont want the same event further up
+  if (checkLeave(e.currentTarget)) {
+    clearDropClasses(e.currentTarget);
+  }
+}
+
+// this is only called when there is no drop event
+function dragEnd(e) {
+  clearDropClasses(e.currentTarget);
+}
+
 function drop(e) {
-  e.stopPropagation();
+  e.stopPropagation(); // dont want the same event further up
   e.preventDefault();
 
-  const item = e.currentTarget;
-  item._kdsDragEnterCount = 0;
-  item.classList.remove('kds-droppable');
+  e.currentTarget._kdsDragEnterCount = 0;
+  clearDropClasses(e.currentTarget);
 
   const fromData = e.dataTransfer.getData('text/plain');
-  const toData = this._getItemId(item);
+  const toData = this._getItemId(e.currentTarget);
+  const dropMode = e.currentTarget._kdsDropMode;
 
   const evt = new CustomEvent('kds-drop', {
     bubbles: true,
     cancelable: true,
     composed: true,
-    detail: { fromId: fromData, toId: toData, dropMode: item.dataset.dropMode }
+    detail: { fromId: fromData, toId: toData, dropMode }
   });
-  item.dispatchEvent(evt);
+  e.currentTarget.dispatchEvent(evt);
 }
 
+// Handles drag/drop events wqhen connected to an element.
+// Drop targets (see dragover) must be descendants of the connected element and must have a non-empty data-drop-mode attribute.
+// Depending on the data-drop-mode value, one of the CSS classes kds-droppable, kds-droppable-before
+//   or kds-droppable-after is set on the connected element on dragenter, for dynamic styling on the connected element.
+// On drop, a custom event 'kds-drop' is dispatched on the connected element.
 class KdsDragDropProvider {
   constructor(getItemId) {
     this._getItemId = getItemId;
@@ -65,6 +136,7 @@ class KdsDragDropProvider {
     this._dragEnter = dragEnter.bind(this);
     this._dragOver = dragOver.bind(this);
     this._dragLeave = dragLeave.bind(this);
+    this._dragEnd = dragEnd.bind(this);
     this._drop = drop.bind(this);
   }
 
@@ -77,7 +149,9 @@ class KdsDragDropProvider {
     h.addEventListener('dragenter', this._dragEnter);
     h.addEventListener('dragover', this._dragOver);
     h.addEventListener('dragleave', this._dragLeave);
+    h.addEventListener('dragend', this._dragEnd);
     h.addEventListener('drop', this._drop);
+    h._kdsDragEnterCount = -1;
   }
 
   disconnect(element) {
@@ -88,6 +162,7 @@ class KdsDragDropProvider {
     h.removeEventListener('dragenter', this._dragEnter);
     h.removeEventListener('dragover', this._dragOver);
     h.removeEventListener('dragleave', this._dragLeave);
+    h.removeEventListener('dragend', this._dragEnd);
     h.removeEventListener('drop', this._drop);
   }
 }
